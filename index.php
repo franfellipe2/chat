0041b68f05c4c8feb74abb49367ef7ef5bb8617d
sie <franfellipe2@hotmail.com>
2017-10-07 16:35:47 -0300

    css basico

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
         <title>Chat com PHP</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Mukta+Vaani|Roboto" rel="stylesheet">
     </head>
     <body>
         <div id="content">
